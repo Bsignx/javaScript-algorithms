0e1004064e70f0e50b2c80c92fd881c50aa01799
Bruno Mariano <bsignx@gmail.com>
2022-08-23 08:27:56 -0300

    feat: create bubbleSort function

---
+++ sorting/bubble-sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([1, 2, -3, 10, 5, 6, 7]));
+
+// Big O Notation: O(n^2)
